2d7a56004311078a2b7cbb8ec4e70845c46d4eb1
bharathy1999 <121527343+bharathy1999@users.noreply.github.com>
2023-11-20 12:26:37 +0530

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -1,32 +1,43 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
 import java.util.Scanner;
+import java.util.Stack;
 public class Main
-{   
-	public static void main(String[] args) {
-	      Scanner sc=new Scanner(System.in);
-	      String a=sc.next();
-	      boolean v=true;
-	      int bl=0;
-	      int br=0;
-	      
-	      for(int i=0;i<a.length;i++){
-	          if(a.charAt(i)=='('){
-	              bl++;
-	          }
-	           if(a.charAt(i)==')'){
-	              br++;
-	          }
-	      }
-	      if(bl!=br){
-	          v=false;
-	      }
-	      
-	      
+{
+   static  boolean isValid(String str){
+           boolean a=true;
+           int bl=0;
+           int br=0;
+           for(int i=0;i<str.length();i++){
+               if(str.charAt(i)=='('){
+                   bl++;
+                           }
+                if(str.charAt(i)==')'){
+                   br++;
+                      }
+           }
+            if(bl!=br){
+               return false;
+           }
+           
+           
+           
+           
+           
+           return a;
+          
+    }
+    
+	  public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a mathematical expression :");
+        String a=sc.next();
+        
+         boolean b=isValid(a);
+         if(b){
+         System.out.print(true);
 	}
+	else{
+        System.out.print(false);
+        }
+}
+
 }
